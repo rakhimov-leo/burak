bcaa758e5e7a15ebadc212bb62700ba8523ce24e
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-06-22 18:01:00 +0900

    feat: connect mongoDB via mongoose

---
+++ src/train.ts
@@ -0,0 +1,29 @@
+console.log("_________________TRAIN AREA________________");
+
+//                              TASK H:
+
+/*
+shunday function tuzing, u integerlardan
+ iborat arrayni argument sifatida qabul qilib,
+  faqat positive qiymatlarni olib string holatda 
+  return qilsin
+
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+*/
+
+//                              TASK G:
+
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+// function getHighestIndex(array: number[]): number {
+//   const max = Math.max(...array);
+//   return array.indexOf(max);
+// }
+
+// console.log(getHighestIndex([5, 21, 12, 21, 8]));
