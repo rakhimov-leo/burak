c00b5b667cd01338e6885d6d904bf3a6cbbfeac8
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-06-25 00:08:06 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1-ENTRANCE **/
+
 const app = express();
 console.log("__dirname:", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
@@ -11,8 +13,12 @@ app.use(express.json());
 /** 2-SESSIONS **/
 
 /** 3-VIEWS **/
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 /** 4-ROUTERS **/
+
+app.use("/", router);
 
 export default app;
