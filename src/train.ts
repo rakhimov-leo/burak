0500c5239309236b765bdee0d89a44013fcd4205
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-06-26 02:09:28 +0900

    feat: TASK-H2 is done

--- src/train.ts
+++ src/train.ts
@@ -1,5 +1,22 @@
 console.log("_________________TRAIN AREA________________");
 
+//                            H2-TASK:
+/*
+Shunday function tuzing, unga string 
+argument pass bolsin. Function ushbu 
+agrumentdagi digitlarni yangi stringda 
+return qilsin
+
+MASALAN: getDigits("m14i1t") return qiladi "141"
+*/
+function getDigits(a: string): string {
+  return a
+    .split("")
+    .filter((char) => char >= "0" && char <= "9")
+    .join("");
+}
+console.log(getDigits("m1vds2wi1t"));
+
 //                            TASK H:
 
 /*
@@ -11,17 +28,19 @@ shunday function tuzing, u integerlardan
 MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 */
 
-function getPositive(array: number[]): string {
-  return array.filter((num) => num > 0).join("");
-}
+// function getPositive(array: number[]): string {
+//   return array.filter((num) => num > 0).join("");
+// }
 
-console.log(getPositive([5, -6, 6, -8]));
+// console.log(getPositive([5, -6, 6, -8]));
 
 //                              TASK G:
 
 // Yagona parametrga ega function tuzing.
-// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
-// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+// Va bu function parametr orqalik integer ma'lumot
+// turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi
+// eng katta qiymatning indeksini qaytarsin.
 
 // MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
 // Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
