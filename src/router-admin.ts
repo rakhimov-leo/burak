d247c4a1ccfe977f9f55e19589382ba5fe580efd
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-07-10 18:43:45 +0900

    feat: upload files into server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 //** Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
@@ -12,7 +13,11 @@ routerAdmin
 
 routerAdmin
   .get("/signup", restaurantController.getSignUp)
-  .post("/signup", restaurantController.processSignup);
+  .post(
+    "/signup",
+    makeUploader("members").single("memberImage"),
+    restaurantController.processSignup
+  );
 
 routerAdmin.get("/logout", restaurantController.logout);
 
@@ -24,8 +29,17 @@ routerAdmin.get(
   restaurantController.verifyRestaurant,
   productController.getAllProducts
 );
-routerAdmin.post("/product/create", productController.createNewProduct);
-routerAdmin.post("/product/:id", productController.updateChosenProduct);
+routerAdmin.post(
+  "/product/create",
+  restaurantController.verifyRestaurant,
+  makeUploader("products").single("productImage"),
+  productController.createNewProduct
+);
+routerAdmin.post(
+  "/product/:id",
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProduct
+);
 
 //** User */
 export default routerAdmin;
