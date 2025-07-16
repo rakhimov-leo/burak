8b2c2f77e351e9a7b2ee1235305e557cf3dc47d7
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-07-17 01:20:10 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,25 @@
 console.log("Signup frontend javascript file");
+
+$(function () {});
+
+function validateSignupForm() {
+  const memberNick = $(".member-nick").val();
+  const memberPhone = $(".member-phone").val();
+  const memberPassword = $(".member-password").val();
+  const confirmPasword = $(".confirm-password").val();
+
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPasword === ""
+  ) {
+    alert("Please insert all required inputs");
+    return false;
+  }
+
+  if (memberPassword !== confirmPasword) {
+    alert("Password differs, please check!");
+    return false;
+  }
+}
