8473c098a1b276e7a280fc0db50fd8afd2707bf2
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-07-18 23:53:34 +0900

    fix: users page render real data

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -115,6 +115,7 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
   try {
     console.log("getUsers");
     const result = await memberService.getUsers();
+    console.log("result:", result);
 
     res.render("users", { users: result });
   } catch (err) {
