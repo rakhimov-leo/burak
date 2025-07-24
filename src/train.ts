7358ec33985bd672530670f5096c40fc51bcc7c1
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-07-24 16:13:34 +0900

    Task R

--- src/train.ts
+++ src/train.ts
@@ -13,6 +13,14 @@ MASALAN: calculate("1 + 3"); return 4;
 1 + 3 = 4, shu sababli 4 natijani qaytarmoqda.
 */
 
+function calculate(expression: string): number {
+  const parts = expression.split("+"); // ["1 ", " 3"]
+  const num1 = parseInt(parts[0].trim());
+  const num2 = parseInt(parts[1].trim());
+  return num1 + num2;
+}
+
+console.log(calculate("1 + 3"));
 
 //                            TASK Q:
 /*
