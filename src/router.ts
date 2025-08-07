d605b5523cdbb7de71f90a277af0fe984dec2878
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-08-07 19:05:49 +0900

    feat: create getRestaurant rest Api

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,7 @@ import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
 
 /** Member **/
+router.get("/member/restaurant", memberController.getRestaurant);
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
 router.post(
@@ -28,7 +29,7 @@ router.post(
   memberController.updateMember
 );
 
-router.get("/member/top-users", memberController.getTopUsers)
+router.get("/member/top-users", memberController.getTopUsers);
 /** Product **/
 
 /** Order **/
