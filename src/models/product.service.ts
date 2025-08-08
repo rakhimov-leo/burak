8addf09c8dd115f1acb857635014f08a7d25e37f
rakhimov-leo <tiktok.odilstudio.uzboom@gmail.com>
2025-08-09 01:06:28 +0900

    feat: develop getProduct rest Api

--- src/models/product.service.ts
+++ src/models/product.service.ts
@@ -10,6 +10,7 @@ import { Message } from "../libs/Errors";
 import { shapeInoMongooseObjectId } from "../libs/config";
 import { ProductStatus } from "../libs/enums/product.enum";
 import { T } from "../libs/types/common";
+import { ObjectId } from "mongoose";
 
 class ProductService {
   private readonly productModel;
@@ -18,6 +19,7 @@ class ProductService {
     this.productModel = ProductModel;
   }
   /** SPA */
+
   public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
     const match: T = { productStatus: ProductStatus.PROCESS };
 
@@ -42,6 +44,22 @@ class ProductService {
       ])
       .exec();
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
+  }
+
+  public async getProduct(
+    memberId: ObjectId | null,
+    id: string
+  ): Promise<Product> {
+    const productId = shapeInoMongooseObjectId(id);
+    let result = await this.productModel.findOne({
+      _id: productId,
+      productStatus: ProductStatus.PROCESS,
+    }).exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    //TODO: If authenticated users => first => view log creation
+
     return result;
   }
 
